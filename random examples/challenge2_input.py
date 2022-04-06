832c498268e93da10695810bba03afc460e2caca
Rafael Rosa <103063877+Cofault@users.noreply.github.com>
2022-04-06 17:32:35 -0500

    Delete challenge2_input.py

--- random examples/challenge2_input.py
+++
@@ -1,4 +0,0 @@
-dia = int(input ("Qual dia do seu nascimento?"))
-mes = str(input ("Qual mes do seu nascimento?"))
-ano = int(input ("Qual ano do seu nascimento?"))
-print ("Entao voce nasceu em {} de {} de {}, esta correto?".format(dia,mes,ano))
